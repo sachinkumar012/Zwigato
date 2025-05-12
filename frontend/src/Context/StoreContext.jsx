26f8712d327a370f99a068acc1d5f54b17a919ac
Sachin Kumar <117492894+sachinkumar012@users.noreply.github.com>
2025-05-12 16:37:05 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://zwigato-backend-30qr.onrender.com"
 
     const [food_list, setFoodList] = useState([]);
     
